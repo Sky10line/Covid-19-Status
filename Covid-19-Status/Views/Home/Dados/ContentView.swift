7153ccbb32cff14fa1867f9b52ec9aac0357fc46
Sky10line <rodrigo_ryo@hotmayo@hotmail.com>
2020-06-03 17:15:45 -0300

    Tela de dados com picker acabado

--- Covid-19-Status/Views/Home/Dados/ContentView.swift
+++ Covid-19-Status/Views/Home/Dados/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State var isPresented = false
     @State var idState: String
     @State private var selectedStateIndex = 0
+    @State private var backOpacity = 1.0
     var body: some View {
         
         let state = DataService.getState(idState)!
@@ -24,16 +25,18 @@ struct ContentView: View {
         return ZStack {
             NavigationView {
                 VStack {
+                    Button(action: {
+                        self.backOpacity = 0.3
+                        withAnimation{
+                            self.isPresented.toggle()
+                        }
+                    }, label: {
+                        Text(allStates[selectedStateIndex].state)
+                            .font(.title)
+                        Image(systemName: "chevron.right").offset(y: 2)
+                    }).offset(x:-110, y: -160)
+                    
                     VStack {
-                        Button(action: {
-                            withAnimation{
-                                self.isPresented.toggle()
-                            }
-                        }, label: {
-                            Text(allStates[self.selectedStateIndex].state)
-                                .font(.title)
-                        }).offset(x: -110,y: -100)
-                        
                         Recovered(recovereds: state.refuses).offset(y:-80)
                         
                         Cases(cases: state.cases, suspects: state.suspects).offset(y: -60)
@@ -42,31 +45,36 @@ struct ContentView: View {
                     }.offset(y:-50)
                     
                     
-                }.navigationBarHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                }.opacity(backOpacity).navigationBarHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
             }
             ZStack{
                 HStack{
                     Spacer()
-                    VStack(alignment: .center, spacing: 18, content: {
-                        Picker(selection: $selectedStateIndex, label: Text("")) {
-                            ForEach(0 ..< allStates.count) {
-                                Text(allStates[$0].state)
+                    VStack(alignment: .center, spacing: 10, content: {
+                        VStack(alignment: .center, spacing: 10, content: {
+                            Text("Selecione um estado:").font(.body).offset(x: 5, y: 30)
+                            Picker(selection: $selectedStateIndex, label: Text("")) {
+                                ForEach(0 ..< allStates.count) {
+                                    Text(allStates[$0].state)
+                                }
                             }
-                        }
+                        }).background(Color.white)
+                        .cornerRadius(10.0)
                         Button(action: {
+                            self.backOpacity = 1.0
                             self.idState = allStates[self.selectedStateIndex].uf
                             withAnimation{
                                 self.isPresented.toggle()
                             }
                             print(String(self.isPresented))
                         }, label: {
-                            Text("Confimar").frame(width: 100.0, height: 3.0).padding()
-                                .background(Color.white).cornerRadius(10.0).foregroundColor(Color.black)
-                        }).offset(x: 5)
+                            Text("Confimar").frame(width: 300.0, height: 10.0).padding()
+                                .background(Color.white).cornerRadius(10.0).foregroundColor(Color.black).offset(x: 5)
+                        }).background(Color.white)
+                        .cornerRadius(10.0)
                     
                     }).frame(width: 287).padding(EdgeInsets(top: 0, leading: 26, bottom: 60, trailing: 34))
-                        .background(Color.gray)
-                        .cornerRadius(10.0).shadow(radius: 50.0)
+                        .shadow(radius: 50.0)
                         
                         
                    Spacer()
