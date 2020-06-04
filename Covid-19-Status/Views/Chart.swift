50c298386cf5251ccd6590f44a9ec08274e0c12a
Rogerio Lucon <rogerios.lucon@gmail.com>
2020-06-04 15:39:02 -0300

    alterado para mostrar os ultimos 7,15,30 dias.

--- Covid-19-Status/Views/Chart.swift
+++ Covid-19-Status/Views/Chart.swift
@@ -10,24 +10,33 @@ import SwiftUI
 
 class ChartViewModel: ObservableObject {
     @Published var selected: Int = 0 {didSet {
-        list = allListst[selected]
+        list = chageList()
+        maxValue = list.max()!
         }}
-    @Published var list: [CGFloat] = []
-    let allListst: [[CGFloat]] = [[10,20,100,200],[40,30,20,10],[10,20,30,20,10]]
+    @Published var list: [CGFloat]
+    @Published var maxValue: CGFloat
+    let fullList: [CGFloat]
     
-    init() {
-        // Exemplo de pegar um dado especifico.
-        let aux:[BrazilianState] = DataService.getStateByDateRange(state: "SP", start: "20200301", end: "20200316")
-        for data in aux {
-            list.append(CGFloat(data.cases))
+    init(list: [CGFloat]) {
+        self.fullList = list
+        self.list = fullList.suffix(7)
+        self.maxValue = fullList.max()!
+    }
+    
+    func chageList() -> [CGFloat]{
+        if selected == 0 {
+            return fullList.suffix(7)
         }
-        list = list.filter {$0 > 0}
+        if selected == 1 {
+            return fullList.suffix(15)
+        }
+        return fullList
     }
 }
 
 struct ChartBarView: View {
     
-    @ObservedObject var viewModel = ChartViewModel()
+    @ObservedObject var viewModel: ChartViewModel
     @State var title: String = "Nome do grafico"
     @State var xTitle: String = "Parametro x"
     @State var yTitle: String = "Parametro y"
@@ -40,52 +49,66 @@ struct ChartBarView: View {
             VStack {
                 Text(title).font(.title)
                 
-                Picker(selection: $viewModel.selected, label: Text("")) {
-                    Text("Mes").tag(0)
-                    Text("Semana").tag(1)
-                    Text("Dia").tag(2)
-                }.pickerStyle(SegmentedPickerStyle())
-                    .padding(.horizontal)
                 VStack(alignment: .leading) {
                     
                     VStack {
                         HStack(alignment: .center) {
                             ZStack {
-                            Text(yTitle)
-                                .frame(width: 150)
-                                .lineLimit(1)
-                                .rotationEffect(Angle(degrees: 90))
+                                Text(yTitle)
+                                    .frame(width: 150)
+                                    .lineLimit(1)
+                                    .rotationEffect(Angle(degrees: 90))
                             }.frame(width: 0)
                             Spacer()
-//                            ScrollView(.horizontal) {
-                            HStack(alignment: .bottom, spacing: 6) {
-                                ForEach(viewModel.list, id: \.self) { i in
-                                    BarsChart(value: i, width: self.getBarWidth(), parentHeigth: 300, maxValue: 200)
-                                }
-                            }
-                            .frame(height: 300)
+                            if viewModel.list.count > 20{
+                                ScrollView(.horizontal) {
+                                    HStack(alignment: .bottom, spacing: 6) {
+                                        ForEach(viewModel.list, id: \.self) { i in
+                                            BarsChart(value: i, width: self.getBarWidth(), parentHeigth: 300, maxValue: self.viewModel.maxValue)
+                                        }
+                                    }
+                                }.frame(height: 300)
                                 .frame(maxWidth: 320)
-//                        }
+                            } else {
+                                HStack(alignment: .bottom, spacing: 6) {
+                                    ForEach(viewModel.list, id: \.self) { i in
+                                        BarsChart(value: i, width: self.getBarWidth(), parentHeigth: 300, maxValue: self.viewModel.maxValue)
+                                    }
+                                }
+                                .frame(height: 300)
+                                .frame(maxWidth: 320)
+                            }
                         }
                         Text(xTitle).padding()
                     }
+                    Picker(selection: $viewModel.selected, label: Text("")) {
+                        Text("Semana").tag(0)
+                        Text("Quinzena").tag(1)
+                        Text("Mes").tag(2)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .padding(.horizontal)
                 }.padding(.vertical)
                     .padding(.horizontal, 30)
             }
         }
         
     }
+    
+    init(list: [CGFloat]) {
+        viewModel = ChartViewModel(list: list)
+    }
+    
     func getBarWidth() -> CGFloat {
         var width: CGFloat = 300
         let listSize: CGFloat = CGFloat(viewModel.list.count)
         let spaces: CGFloat = 6 * listSize
         width -= spaces
         let elementSize = (width / listSize)
-//        print("Tamanho da tela \(UIScreen.main.bounds.size.width)")
-//        print("Tamanho da lista \(CGFloat(viewModel.list.count))")
-//        print("Tela reduzida \(width)")
-//        print("Tamanho do elemento\(elementSize)")
-//        print("Tamanho Total Utilizado\(elementSize * listSize)")
+        //        print("Tamanho da tela \(UIScreen.main.bounds.size.width)")
+        //        print("Tamanho da lista \(CGFloat(viewModel.list.count))")
+        //        print("Tela reduzida \(width)")
+        //        print("Tamanho do elemento\(elementSize)")
+        //        print("Tamanho Total Utilizado\(elementSize * listSize)")
         return elementSize
         
     }
@@ -102,6 +125,8 @@ struct BarsChart: View {
         var w:CGFloat = width
         if width > 30 {
             w = 30
+        } else if width < 10 {
+             w = 10
         }
         let multiplier: CGFloat = (self.maxValue / parentHeigth)
         return ZStack(alignment: .bottom) {
@@ -117,7 +142,7 @@ struct BarsChart: View {
 
 struct ChartBarView_Previews: PreviewProvider {
     static var previews: some View {
-        ChartBarView()
+        ChartBarView(list: [1,2,3,4,5,6,7,14,21,42,82,100,120,160,200,1,2,3,4,5,6,7,14,21,42,82,100,120,160,300])
     }
 }
 
